1dff16e878ae503c5b0e77891079441e44e519b0
jackhadi <akhyatf706@gmail.com>
2018-04-25 20:22:27 -0700

    Initial commit

---
+++ src/praktek03/hadi/Praktek03Hadi.java
@@ -0,0 +1,12 @@
+package praktek03.hadi;
+
+public class Praktek03Hadi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
